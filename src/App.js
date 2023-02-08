01d176b52ffd0067fb37db8c88002c3a5a6ce8a2
unknown <zahura20319@gmail.com>
2023-02-07 16:27:39 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>logic form</h1>
+      <label htmlFor="">full Name: </label>
+      <input type="text" /><br /><br />
+      <label htmlFor="">Father Name: </label>
+      <input type="text" /><br /><br />
+      <label htmlFor="">Mother Name: </label>
+      <input type="text" /> <br /><br />
+      <label htmlFor="">Password</label>
+      <input type="number" /><br /><br />
+      <label htmlFor="">email</label>
+      <input type="email" /><br /><br />
+      <label htmlFor="">Mobile No</label>
+      <input type="number" /> <br /><br />
+      <button>Sumbit</button>
+    </div>
+    
+  );
+}
+
+export default App;
